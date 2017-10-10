82e16cde6bd7671898d1f9806c75585522a7bc5c
a-rusak <alexander.rusak@gmail.com>
2017-10-11 00:22:08 +0300

    Тест подсчёта набранных баллов

--- js/data/game-data.test.js
+++ js/data/game-data.test.js
@@ -1,22 +1,89 @@
 import assert from "assert";
-import {QUESTIONS_COUNT, resultsCount} from "./game.data";
+import {resultsCount, MAX_ERRORS_COUNT} from "./game.data";
 
 const results = [
   {
     attempt: [1, 2, -2],
-    rest: 1
+    rest: 3,
+    ponts: -1
   },
   {
     attempt: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    rest: 3
+    rest: 4,
+    ponts: 10
+  },
+  {
+    attempt: [1, 1, 1, 1, 1, 1, -2, 1, 1, -2],
+    rest: 2,
+    ponts: 4
+  },
+  {
+    attempt: [-2, -2, -2, -2],
+    rest: 0,
+    ponts: -1
+  },
+  {
+    attempt: [],
+    rest: 4,
+    ponts: -1
   }
 ];
 
+const statistics = [4, 5, 8, 10, 11];
+const games = [
+  {
+    points: -1,
+    rest: 0,
+    timeLeft: 200
+  },
+  {
+    points: -1,
+    rest: 3,
+    timeLeft: 0
+  },
+  {
+    points: 10,
+    rest: 4,
+    timeLeft: 100
+  }
+];
+
+const text = testData => `
+  Игрок отвечал на ${testData.attempt.length} вопрос(ов|а).
+  Сделал ${MAX_ERRORS_COUNT - testData.rest} ошиб(ки|ок|ку).
+  ${testData.ponts === -1
+    ? `и проиграл`
+    : `и набрал ${testData.ponts} балл(ов|а)`}
+`;
+
+const phrases = {
+  timeIsUp: () => `Время вышло! Вы не успели отгадать все мелодии`,
+  noMoreAttempts: () =>
+    `У вас закончились все попытки. Ничего, повезёт в следующий раз!`,
+  win: ({place, playersCount, betterThan}) =>
+    `Вы заняли ${place}-ое место из ${playersCount} игроков. Это лучше чем у ${betterThan} игроков`
+};
+
 describe(`Результаты игр`, () => {
-  it(`игрок не ответил на ${QUESTIONS_COUNT} вопросов`, () => {
-    assert.equal(-1, resultsCount(results[0]));
+  describe(`Функция подсчёта набранных баллов игрока`, () => {
+    for (let result of results) {
+      makeTest(result);
+    }
+    function makeTest(result) {
+      it(text(result), () => {
+        assert.equal(result.ponts, resultsCount(result));
+      });
+    }
   });
-  it(`Игрок ответил правильно и не быстро на ${QUESTIONS_COUNT} вопросов и ни разу не ошибся`, () => {
-    assert.equal(10, resultsCount(results[1]));
+
+  describe(`Функция вывода результата игрока`, () => {
+    for (let game of games) {
+      makeTest(game);
+    }
+    function makeTest(game) {
+      it(text(result), () => {
+        assert.equal(result.ponts, resultsCount(result));
+      });
+    }
   });
 });
